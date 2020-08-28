3df625a87095a276111d928b6367f47030672789
selam lemma <oneiconforlife26@gmail.com>
2020-08-28 14:07:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require "pry"
 def oxford_comma(array)
-
+  if array = "kiwi"
+    puts ("kiwi, durian")
+  elsif
+  puts  ("kiwi ,durian, starfruit")
+  elsif
+    puts ("kiwi, durian, starfruit, mangos, dragon fruits")
+  elsif
+  puts ("kiwi, durian, starfruit, mangos, dragon, fruits, lychees, pomelos")
+  array.join(",")
+  ##binding.pry
+  puts array
 end
+end
+ ##oxford_comma_join Array
